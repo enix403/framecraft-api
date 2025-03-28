728c824014292f44f980b36f9dd09c5beabbac71
enix403 <fog.code000@gmail.com>
2025-03-28 16:15:33 +0500

    changes

--- src/datasources/mongodb.ts
+++ src/datasources/mongodb.ts
@@ -1,13 +1,13 @@
 import { green, red } from "colorette";
-import mongoose from "mongoose";
+import mongoose, { type ConnectOptions } from "mongoose";
 
 import { appEnv } from "@/lib/app-env";
 import { appLogger } from "@/lib/logger";
 
-export async function connectMongoDB() {
+export async function connectMongoDB(options?: ConnectOptions) {
   try {
     appLogger.verbose("Connecting to MongoDB ...");
-    await mongoose.connect(appEnv.MONGO_URL ?? "");
+    await mongoose.connect(appEnv.MONGO_URL ?? "", options);
     appLogger.verbose(green("Connected successfully to MongoDB instance"));
   } catch (error) {
     appLogger.error(red("Connection to MongoDB instance failed"));
--- src/scripts/repl.ts
+++ src/scripts/repl.ts
@@ -1,24 +1,58 @@
 import "./repl-effects";
 
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
+/* ========================== */
 
 import { appEnv } from "@/lib/app-env";
+import { appLogger } from "@/lib/logger";
 
-console.log("TypeScript REPL started\n");
+import { validateJoiSchema } from "@/middleware/validation";
 
-// Start the REPL
-const r = require("node:repl").start({
-  prompt: "\x1b[34m>>\x1b[0m ",
-  useGlobal: true
-});
+import { connectMongoDB } from "@/datasources/mongodb";
 
-// Define custom REPL commands
-r.defineCommand("cls", {
-  help: "Clears the screen",
-  action() {
-    console.clear();
-    this.displayPrompt();
+import { DisposableToken } from "@/models/disposable-token";
+import { Plan } from "@/models/plan";
+import { User } from "@/models/user";
+
+import { mailPresets } from "@/mailer/mailer";
+
+import { comparePassword, hashPassword } from "@/features/auth/hashing";
+import { tokenService } from "@/features/auth/token.service";
+
+/* ========================== */
+
+async function bootstrap() {
+  try {
+    const timeout = 100;
+    await connectMongoDB({
+      timeoutMS: timeout,
+      socketTimeoutMS: timeout,
+      connectTimeoutMS: timeout,
+      waitQueueTimeoutMS: timeout,
+      serverSelectionTimeoutMS: timeout
+    });
+  } catch (error) {
+    // ignore if cannot connect
   }
-});
+
+  console.log("TypeScript REPL started\n");
+
+  // Start the REPL
+  const r = require("repl").start({
+    prompt: "\x1b[34m>>\x1b[0m ",
+    useGlobal: true
+  });
+
+  // Define custom REPL commands
+  r.defineCommand("cls", {
+    help: "Clears the screen",
+    action() {
+      console.clear();
+      this.displayPrompt();
+    }
+  });
+
+  r.on("exit", () => {
+    process.exit();
+  });
+}
+bootstrap();
