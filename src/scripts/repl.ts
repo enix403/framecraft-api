cc75b807306b665dec9c9ac697c73d1facaa4e3e
enix403 <fog.code000@gmail.com>
2025-03-28 15:59:29 +0500

    repl

--- src/scripts/repl.ts
+++ src/scripts/repl.ts
@@ -1,13 +1,24 @@
-require("clear-any-console")();
+import "./repl-effects";
+
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+
+import { appEnv } from "@/lib/app-env";
 
 console.log("TypeScript REPL started\n");
 
-import * as fs from 'fs';
-import * as path from 'path';
-import * as os from 'os';
-import { appEnv } from "@/lib/app-env";
+// Start the REPL
+const r = require("node:repl").start({
+  prompt: "\x1b[34m>>\x1b[0m ",
+  useGlobal: true
+});
 
-require('node:repl').start({
-    prompt: ">> ",
-    useGlobal: true
+// Define custom REPL commands
+r.defineCommand("cls", {
+  help: "Clears the screen",
+  action() {
+    console.clear();
+    this.displayPrompt();
+  }
 });
