ea7d1c12f7902a0d0cffc070c910c152ea4a6ee8
enix403 <fog.code000@gmail.com>
2025-04-11 19:44:16 +0500

    image start script

---
+++ image_start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ENV_FILE=".env"
+
+# Create a temporary file
+envfile=$(mktemp)
+
+# Ensure the temp file is deleted on exit, regardless of success or error
+cleanup() {
+  rm -f "$envfile"
+}
+trap cleanup EXIT
+
+# Populate the temp env file using Node.js
+node -e "
+  Object.entries(require('dotenv').parse(require('fs').readFileSync('$ENV_FILE')))
+    .forEach(([k, v]) => console.log(\`\${k}=\${v}\`));
+" > "$envfile"
+
+# Run the podman container and capture its exit code
+podman run --env-file "$envfile" -p 3001:80 framecraft/api
+exit_code=$?
+
+# Exit with the same status as the podman run command
+exit $exit_code
